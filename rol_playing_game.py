03cc32133e451ddc30bea25772f4f7cabb66bba1
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-11-14 12:22:24 -0600

    Segunda versión de las clases Aventurero y HombreLobo

--- rol_playing_game.py
+++ rol_playing_game.py
@@ -19,14 +19,16 @@ class Aventurero:
         """Propiedad de solo lectura que devuelve la vitalidad de este
         aventurero."""
         return self.__vitalidad
-
+    
     def pelea(self, monstruo):
         """Realizar una pelea entre este aventurero y un monstruo. Si el
         monstruo gana, la vitalidad se reduce a la mitad, de lo contrario
         la fortuna crece por 20 puntos."""
         if monstruo.vence():
+            print(f'Ganó {monstruo}')
             self.__vitalidad //= 2
         else:
+            print(f'Perdió {monstruo}')
             self.__fortuna += 20
 
     def __str__(self):
@@ -40,27 +42,53 @@ Mi fortuna es: {self.__fortuna}.''')
 
 class HombreLobo:
     """Una instancia de esta clase representa un hombre lobo."""
+    
+    def __init__(self, nombre):
+        """Inicializa uan instancia de la clase HombreLobo."""
+        self.nombre = nombre
+        self.__fuerza = [True, False]
+        
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, valor):
+        if type(valor) == str and valor != '':
+            self.__nombre = valor
+        else:
+            raise Exception(f'Nombre no válido: "{valor}"')
+    
+    @property
+    def fuerza(self):
+        return len(self.__fuerza)
 
     def vence(self):
         """Devuelve True o False de manera aleatoria pare indicar si este 
         hombre lobo venció a un adversario."""
-        return choice([True, False])
-
+        resultado = choice(self.__fuerza)
+        if resultado:
+            self.__fuerza.append(True)
+        else:
+            self.__fuerza = [False]
+        return resultado
+    
+    def __str__(self):
+        return f'Hombre Lobo: {self.nombre}'
 
 # -------------------------------------------------------------------
 # Código de prueba
         
-a1 = Aventurero('Juan sin miedo')
-a2 = Aventurero('Indiana Jones')
-lobo1 = HombreLobo()
+def juego():
+    aventurero = Aventurero('Abraham Van Helsing')
+    hombre_lobo = HombreLobo('Larry Talbot')
+    
+    contador = 1
+    while aventurero.vitalidad > 0 and contador <= 20:
+        print('A punto de pelear...')
+        aventurero.pelea(hombre_lobo)
+        print(aventurero)
+        contador += 1
+    print(f'Fuerza del hombre lobo: {hombre_lobo.fuerza}')
 
-print(a1)
-print()
-print(a2)
-print()
-
-print('Peleando con un hombre lobo...')
-print()
-a1.pelea(lobo1)
-print('Resultado:')
-print(a1)
+juego()
